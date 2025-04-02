cd508926e2ad666f774c2b5cccfc9d64fddad4cb
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-04-02 20:59:34 +0200

    added console output to xunit bootstrap

--- HttpClient/HttpClient/HttpClientTests.cs
+++ HttpClient/HttpClient/HttpClientTests.cs
@@ -1,14 +1,15 @@
 ﻿using Xunit;
+using Xunit.Abstractions;
 
 namespace HttpClient;
 
-public class HttpClientTests
+public class HttpClientTests(ITestOutputHelper output)
 {
     [Fact]
     public async Task ShouldReturnMessage()
     {
         var client = new HttpClient();
-
+        
         var message = await client.GetMessage(":employee:name:");
         
         Assert.Equal("Hello :employee:name:!", message);
