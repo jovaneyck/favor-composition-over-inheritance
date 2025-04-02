5a0254a531d32bb844405a64029e959ebf7f54a8
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-04-02 20:54:16 +0200

    prep exercise

---
+++ HttpClient/HttpClient/HttpClient.cs
@@ -0,0 +1,14 @@
+﻿namespace HttpClient;
+
+public interface IHttpClient
+{
+    public Task<string> GetMessage(string employeeName);
+}
+
+public class HttpClient : IHttpClient
+{
+    public Task<string> GetMessage(string employeeName)
+    {
+        return Task.FromResult($"Hello {employeeName}!");
+    }
+}
---
+++ HttpClient/HttpClient/HttpClientTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace HttpClient;
+
+public class HttpClientTests
+{
+    [Fact]
+    public async Task ShouldReturnMessage()
+    {
+        var client = new HttpClient();
+
+        var message = await client.GetMessage(":employee:name:");
+        
+        Assert.Equal("Hello :employee:name:!", message);
+    }
+}
